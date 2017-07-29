5e045df2c540627e90eed3e0048ae7ba5ecadf8e
Julian Maingot <julianmaingot@gmail.com>
2017-07-28 22:24:10 -0400

    Added method for area

--- projects/area/src/main.rs
+++ projects/area/src/main.rs
@@ -9,6 +9,13 @@ fn area(r: &Rect) -> u32 {
     r.len * r.wid
 }
 
+impl Rect {
+    // return the area of the rectangle structure
+    fn area(&self) -> u32 {
+        self.len * self.wid
+    }
+}
+
 // calculate area for rectangle represented with tuple
 fn tuple_area(dim: &(u32, u32)) -> u32 {
     dim.0 * dim.1
@@ -24,6 +31,8 @@ fn main() {
     println!("struct area: {}", area(&rect));
     println!("debug rect: {:?}", &rect);
     println!("debug rect: {:#?}", &rect);
+
+    println!("struct method area: {}", rect.area());
 
     let rect = (3, 6);
     println!("tuple area: {}", tuple_area(&rect));
