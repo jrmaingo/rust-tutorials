c831611f86bf46c14190c27a0e56ceb831161971
Julian Maingot <julianmaingot@gmail.com>
2017-07-28 21:44:00 -0400

    Added alternative area calculations

--- projects/area/src/main.rs
+++ projects/area/src/main.rs
@@ -8,7 +8,23 @@ fn area(r: &Rect) -> u32 {
     r.len * r.wid
 }
 
+// calculate area for rectangle represented with tuple
+fn tuple_area(dim: &(u32, u32)) -> u32 {
+    dim.0 * dim.1
+}
+
+// returns the area for rectangle represented by params
+fn param_area(len: u32, wid: u32) -> u32 {
+    len * wid
+}
+
 fn main() {
     let rect = Rect { len: 5, wid: 7 };
-    println!("{}", area(&rect))
+    println!("struct area: {}", area(&rect));
+
+    let rect = (3, 6);
+    println!("tuple area: {}", tuple_area(&rect));
+
+    let (len, wid) = (4, 5);
+    println!("param area: {}", param_area(len, wid));
 }
