18d54ab5a391b0ee7ec3de00e1560441793dcbb2
Julian Maingot <julianmaingot@gmail.com>
2017-09-17 15:08:57 -0400

    Finished 'echo'

--- projects/echo/src/main.rs
+++ projects/echo/src/main.rs
@@ -1,9 +1,27 @@
 use std::io;
+use std::env;
+
+const NOARGS: usize = 0;
 
 fn main() {
+    let args = &env::args().collect::<Vec<String>>()[1..];
     let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => print!("{}", input),
-        Err(error) => println!("error: {}", error),
-    }
+
+    match args.len() {
+        NOARGS => {
+            println!("no args given, enter content now:");
+            // get input if no args given
+            match io::stdin().read_line(&mut input) {
+                Err(error) => println!("error: {}", error),
+                _ => (),
+            }
+        },
+        _ => {
+            for elem in args {
+                input.push_str(elem.as_str());
+            }
+        },
+    };
+
+    println!("{}", input.trim());
 }
