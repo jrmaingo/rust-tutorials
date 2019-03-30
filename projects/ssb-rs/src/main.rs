52e5635a73e93db1516d99484966ab69d2a44f7b
Julian Maingot <julianmaingot@gmail.com>
2019-03-30 17:50:25 -0400

    Able to read JSON from ~/.ssb/secret

--- projects/ssb-rs/src/main.rs
+++ projects/ssb-rs/src/main.rs
@@ -1,3 +1,35 @@
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+struct SsbSecret {
+    curve: String,
+    //keypair: Keypair,
+    feed_id: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("/Users/julian/.ssb/secret")
+        .expect("Unable to read secret file");
+    let json_str = BufReader::new(file)
+        .lines()
+        .filter_map(|line_res| {
+            let line = line_res.unwrap();
+            if !line.starts_with("#") {
+                Some(line)
+            } else {
+                None
+            }
+        })
+        .fold(String::new(), |mut acc, line| {
+            acc.push_str(&line);
+            acc
+        });
+    let json: Value = serde_json::from_str(&json_str)
+        .expect("failed to parse secret file");
+    println!("curve: {}", json["curve"]);
+    // let key = Keypair::from_bytes(
 }
