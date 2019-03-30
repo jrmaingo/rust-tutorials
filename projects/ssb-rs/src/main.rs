d00eb5eb0025f3c441b2a53f4bf73146c9c776f8
Julian Maingot <julianmaingot@gmail.com>
2019-03-30 18:27:17 -0400

    Successfully parsed secret key

--- projects/ssb-rs/src/main.rs
+++ projects/ssb-rs/src/main.rs
@@ -1,5 +1,7 @@
+use base64::decode;
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
+use sodiumoxide::crypto::sign::ed25519::*;
 
 use std::fs::File;
 use std::io::BufReader;
@@ -7,11 +9,15 @@ use std::io::prelude::*;
 
 struct SsbSecret {
     curve: String,
-    //keypair: Keypair,
+    secret_key: SecretKey,
+    public_key: PublicKey,
     feed_id: String,
 }
 
 fn main() {
+    let mut ssb_secret: SsbSecret;
+
+    // extract object for file, parse as JSON
     let file = File::open("/Users/julian/.ssb/secret")
         .expect("Unable to read secret file");
     let json_str = BufReader::new(file)
@@ -30,6 +36,16 @@ fn main() {
         });
     let json: Value = serde_json::from_str(&json_str)
         .expect("failed to parse secret file");
-    println!("curve: {}", json["curve"]);
-    // let key = Keypair::from_bytes(
+
+    // parse secret key from file
+    let secret_key_enc_bytes = json["private"].as_str()
+        .expect("secret key is not a string")
+        .splitn(2, ".").next().unwrap()
+        .as_bytes();
+    let secret_key_dec_bytes = decode(secret_key_enc_bytes)
+        .expect("failed to decode secret key");
+    ssb_secret.secret_key = SecretKey::from_slice(&secret_key_dec_bytes)
+        .expect("failed to create secret key from binary data");
+
+    // TODO: read public key and test!
 }
