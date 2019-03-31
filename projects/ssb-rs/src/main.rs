b5c22e62951c59807f50c8b2e5f4c403f4c17a89
Julian Maingot <julianmaingot@gmail.com>
2019-03-31 00:08:31 -0400

    Move helper inside 'new' func

--- projects/ssb-rs/src/main.rs
+++ projects/ssb-rs/src/main.rs
@@ -17,48 +17,48 @@ struct SsbSecret {
     id: String,
 }
 
-fn get_json_secret(file_path: &str) -> Value {
-    // extract object for file, parse as JSON
-    let file = File::open(file_path)
-        .expect("Unable to read secret file");
-    let json_str = BufReader::new(file)
-        .lines()
-        .filter_map(|line_res| {
-            let line = line_res.unwrap();
-            if !line.starts_with("#") {
-                Some(line)
-            } else {
-                None
-            }
-        })
-        .fold(String::new(), |mut acc, line| {
-            acc.push_str(&line);
-            acc
-        });
-    serde_json::from_str(&json_str)
-        .expect("failed to parse secret file")
-}
-
 impl SsbSecret {
-    fn parse_key(json_secret: &Value, key_name: &str) -> Vec<u8> {
-        let key_enc_bytes = json_secret[key_name].as_str()
-            .expect("key is not a string")
-            .splitn(2, ".").next().unwrap()
-            .as_bytes();
-        decode(key_enc_bytes)
-            .expect("failed to decode key")
-    }
-
     fn new(file_path: &str) -> SsbSecret {
+        fn get_json_secret(file_path: &str) -> Value {
+            // extract object for file, parse as JSON
+            let file = File::open(file_path)
+                .expect("Unable to read secret file");
+            let json_str = BufReader::new(file)
+                .lines()
+                .filter_map(|line_res| {
+                    let line = line_res.unwrap();
+                    if !line.starts_with("#") {
+                        Some(line)
+                    } else {
+                        None
+                    }
+                })
+                .fold(String::new(), |mut acc, line| {
+                    acc.push_str(&line);
+                    acc
+                });
+            serde_json::from_str(&json_str)
+                .expect("failed to parse secret file")
+        }
+
+        fn parse_key(json_secret: &Value, key_name: &str) -> Vec<u8> {
+            let key_enc_bytes = json_secret[key_name].as_str()
+                .expect("key is not a string")
+                .splitn(2, ".").next().unwrap()
+                .as_bytes();
+            decode(key_enc_bytes)
+                .expect("failed to decode key")
+        }
+
         let json_secret: Value = get_json_secret(file_path);
 
         // parse secret key from file
-        let secret_key_dec_bytes = SsbSecret::parse_key(&json_secret, "private");
+        let secret_key_dec_bytes = parse_key(&json_secret, "private");
         let secret_key = SecretKey::from_slice(&secret_key_dec_bytes)
             .expect("failed to create secret key from binary data");
 
         // parse public key from file
-        let public_key_dec_bytes = SsbSecret::parse_key(&json_secret, "public");
+        let public_key_dec_bytes = parse_key(&json_secret, "public");
         let public_key = PublicKey::from_slice(&public_key_dec_bytes)
             .expect("failed to create public key from binary data");
 
